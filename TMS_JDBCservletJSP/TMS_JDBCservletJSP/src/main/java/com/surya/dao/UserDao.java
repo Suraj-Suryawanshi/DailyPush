5341468a4a1aa09983f0dd14674f88af561a8322
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2024-02-29 19:25:40 +0530

    Delete TMS_JDBCservletJSP/TMS_JDBCservletJSP directory

--- TMS_JDBCservletJSP/TMS_JDBCservletJSP/src/main/java/com/surya/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.surya.dao;
-
-import com.surya.model.User;
-
-public interface UserDao {
-
-	boolean addUser(User user);
-	boolean isValidUser(String role, String email, String password);
-}
